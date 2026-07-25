8749ba746ca2b1fffa749c2433ffeb37f5b6a9e0
mtarabet72 <dreamzara76@gmail.com>
2026-07-25 08:33:33 +0100

    Update history.js

--- src/lib/history.js
+++ src/lib/history.js
@@ -2,15 +2,32 @@ import { Query } from "appwrite";
 import { databases, DATABASE_ID, COLLECTIONS } from "./appwrite";
 
 export const EQUIPMENT_LABELS = {
-  presse_cafe: "Presse à café",
-  moulin_barista: "Moulin café barista",
-  moulin_vrac: "Moulin café vrac",
-  machine_espresso: "Machine espresso",
-  autre: "Autre",
+  fr: {
+    presse_cafe: "Presse à café",
+    moulin_barista: "Moulin café barista",
+    moulin_vrac: "Moulin café vrac",
+    machine_espresso: "Machine espresso",
+    autre: "Autre",
+  },
+  ar: {
+    presse_cafe: "آلة ضغط القهوة",
+    moulin_barista: "مطحنة قهوة باريستا",
+    moulin_vrac: "مطحنة قهوة بالجملة",
+    machine_espresso: "آلة إسبريسو",
+    autre: "أخرى",
+  },
+  zgh: {
+    presse_cafe: "ⵜⴰⵎⵙⵙⵓⴹⴹⴰⵜ ⵏ ⵍⵇⴰⵀⵡⴰ",
+    moulin_barista: "ⵜⴰⵙⵉⵔⵜ ⵏ ⵍⵇⴰⵀⵡⴰ ⴱⴰⵔⵉⵙⵜⴰ",
+    moulin_vrac: "ⵜⴰⵙⵉⵔⵜ ⵏ ⵍⵇⴰⵀⵡⴰ ⵙ ⵓⵙⵎⵓⵜⵜⴳ",
+    machine_espresso: "ⵜⴰⵎⵙⵙⵓⴷⵙⵜ ⵏ ⵉⵙⴱⵔⵉⵙⵓ",
+    autre: "ⵢⴰⴹⵏ",
+  },
 };
 
-export function equipmentLabel(type) {
-  return EQUIPMENT_LABELS[type] || type;
+export function equipmentLabel(type, lang = "fr") {
+  const table = EQUIPMENT_LABELS[lang] || EQUIPMENT_LABELS.fr;
+  return table[type] || type;
 }
 
 export async function listTransactions(clientId) {
